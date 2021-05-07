e197c4e447191be9b9ca6d56aa636015e82e5f00
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-05-07 18:41:21 +0800

    edit

--- polymorphism_thru_duck_typing.rb
+++ polymorphism_thru_duck_typing.rb
@@ -1,53 +1,40 @@
 class MathOP
-
+    def initialize(num1,num2)
+        @num1 = num1
+        @num2 = num2
+    end
     def operation(operand)
-        operand.operation
+        operand.operation(@num1,@num2)
     end
 end
 
 class Addition
-    def initialize(num1,num2)
-        @num1 = num1
-        @num2 = num2
-    end
-    def operation
-        @num1 + @num2
+    def operation(num1,num2)
+        num1 + num2
     end
 end
 
 class Subtraction
-    def initialize(num1,num2)
-        @num1 = num1
-        @num2 = num2
-    end
-    def operation
-        @num1 - @num2
+    def operation(num1,num2)
+        num1 - num2
     end
 end
 
 class Multiplication
-    def initialize(num1,num2)
-        @num1 = num1
-        @num2 = num2
-    end
-    def operation
-        @num1 * @num2
+    def operation(num1,num2)
+        num1 * num2
     end
 end
 class Division
-    def initialize(num1,num2)
-        @num1 = num1
-        @num2 = num2
-    end
-    def operation
-        @num1 / @num2
+    def operation(num1,num2)
+        num1 / num2
     end
 end
-math = MathOP.new
-addition = Addition.new(5,7)
-subtraction = Subtraction.new(7,5)
-multiplication = Multiplication.new(7,5)
-division = Division.new(30,5)
+math = MathOP.new(10,5)
+addition = Addition.new
+subtraction = Subtraction.new
+multiplication = Multiplication.new
+division = Division.new
 
 puts math.operation(addition)
 puts math.operation(subtraction)
