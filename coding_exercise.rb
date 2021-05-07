a6b0c2ae1622d0e7a33440fe7f2694edf6a556d5
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-05-07 19:17:14 +0800

    coding

---
+++ coding_exercise.rb
@@ -0,0 +1,10 @@
+def count_pos_sum_neg(arr)
+    positives = arr.select{|num| num > -1 }
+    negatives = arr.select{|num| num < 0 }
+
+    return [positives.count, negatives.sum]
+
+end
+
+print count_pos_sum_neg([1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15])
+puts "\n"
