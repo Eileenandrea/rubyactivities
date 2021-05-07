ed19c9fd215684da024b21dccd283f6f4176ec95
Eileenandrea <eileendeguzman.edg@gmail.com>
2021-05-07 19:21:54 +0800

    coding

--- coding_exercise.rb
+++ coding_exercise.rb
@@ -6,5 +6,5 @@ def count_pos_sum_neg(arr)
 
 end
 
-print count_pos_sum_neg([1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15])
+print count_pos_sum_neg([0,1,2,3,4,5,6,7,8,9,10,-11,-12,-13,-14,-15])
 puts "\n"
